ffd7982f0b9f5aa47864f6dfa086cb36d9d177f8
Sarah Wylie <sarahannemariawylie@gmail.com>
2025-08-22 21:14:03 -0500

    updated photo layout, working on profiles

---
+++ src/app/Hippo/page.tsx
@@ -0,0 +1,94 @@
+const hippos = [
+    {
+        "id": 1,
+        "title": "Eyes on You",
+        "pic": "/puppalooza/hippo/Hippo1.jpeg",
+        "date": "16 August 2025"
+    },
+    {
+        "id": 2,
+        "title": "Blep",
+        "pic": "/puppalooza/hippo/Hippo3.jpeg",
+        "date": "16 August 2025"
+    },
+    {
+        "id": 3,
+        "title": "Nose-Picking",
+        "pic": "/puppalooza/hippo/Hippo6.jpeg",
+        "date": "16 August 2025"
+    },
+    {
+        "id": 4,
+        "title": "Some Cuddles",
+        "pic": "/puppalooza/hippo/cuddleHip.jpg",
+        "date": "4 August 2025"
+    },
+    {
+        "id": 5,
+        "title": "Sleepy Baby",
+        "pic": "/puppalooza/hippo/TinyHippo.jpg",
+        "date": "31 July 2025"
+    },
+    {
+        "id": 6,
+        "title": "Softest Nose",
+        "pic": "/puppalooza/hippo/SoftestNose.jpg",
+        "date": "30 July 2025"
+    },
+    {
+        "id": 7,
+        "title": "Piercing Eyes",
+        "pic": "/puppalooza/hippo/Piercing.jpg",
+        "date": "30 July 2025"
+    },
+    {
+        "id": 8,
+        "title": "One Day Old!",
+        "pic": "/puppalooza/hippo/hippo.jpg",
+        "date": "13 July 2025"
+    },
+];
+export default function Hippo() {
+    return (
+        <main className="flex items-center justify-center pt-16 pb-4">
+            <div className="flex-1 flex flex-col items-center gap-12 min-h-0">
+                <header className="flex flex-col items-center gap-9">
+                    <h1 className="text-5xl font-semibold text-gray-900 dark:text-gray-100">
+                        Hippo
+                    </h1>
+                </header>
+                <div className="max-w-[80%] w-full space-y-6 px-4 flex flex-col items-center">
+                    <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
+
+
+                    </p>
+                    <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
+
+
+                    </p>
+                </div>
+                <div className="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1 gap-4 place-items-center">
+                    {hippos.map((hippo) => (
+                        <div key={hippo.id} className="rounded-xl p-2 m-2">
+                            <span>
+                                <img
+                                    src={hippo.pic}
+                                    alt={hippo.title}
+                                    className="rounded-xl"
+                                />
+                            </span>
+                            <p className="text-base text-gray-900 dark:text-gray-100 font-bold text-center">
+                                {hippo.title}
+                            </p>
+                            <p className="text-sm text-gray-900 dark:text-gray-100 text-center">
+                                {hippo.date}
+                            </p>
+                            <br />
+                            <br />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </main>
+    )
+};
---
+++ src/app/Kathleen/page.tsx
@@ -0,0 +1,80 @@
+const kathleens = [
+    {
+        "id": 1,
+        "title": "Sleepy",
+        "pic": "/puppalooza/kathleen/SleepyKathleen.jpg",
+        "date": "7 August 2025"
+    },
+    {
+        "id": 2,
+        "title": "Yawns",
+        "pic": "/puppalooza/kathleen/Kathleen2.jpeg",
+        "date": "16 August 2025"
+    },
+    {
+        "id": 3,
+        "title": "Thoughtful",
+        "pic": "/puppalooza/kathleen/Kathleen4.jpeg",
+        "date": "16 August 2025"
+    },
+    {
+        "id": 4,
+        "title": "With Uncle Rusty",
+        "pic": "/puppalooza/kathleen/KathleenRusty.jpg",
+        "date": "16 August 2025"
+    },
+    {
+        "id": 5,
+        "title": "Some Light Construction",
+        "pic": "/puppalooza/kathleen/KathleenTheBuilder.jpg",
+        "date": "16 August 2025"
+    },
+    {
+        "id": 6,
+        "title": "2.5 weeks old",
+        "pic": "/puppalooza/kathleen/TinyKathleen.jpg",
+        "date": "30 July 2025"
+    },
+];
+export default function Kathleen() {
+    return (
+        <main className="flex items-center justify-center pt-16 pb-4">
+            <div className="flex-1 flex flex-col items-center gap-12 min-h-0">
+                <header className="flex flex-col items-center gap-9">
+                    <h1 className="text-5xl font-semibold text-gray-900 dark:text-gray-100">
+                        Kathleen
+                    </h1>
+                </header>
+                <div className="max-w-[80%] w-full space-y-6 px-4 flex flex-col items-center">
+                    <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
+                        Kathleen is a feisty pup with no fear. She loves cuddles, being the center of attention, and bossing around bigger dogs. When not taking control of a situation, Kathleen is usually playing hard. Her boss-babe-party-girl attitude, combined with the bikini top on her back, all combine to explain the origin of her name: her namesake and hero Kathleen Hanna.
+                    </p>
+                    <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
+                        Kathleen loves having buddies, so she would do best in a home with other animals.
+                    </p>
+                </div>
+                <div className="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1 gap-4 place-items-center">
+                    {kathleens.map((kathleen) => (
+                        <div key={kathleen.id} className="rounded-xl p-2 m-2">
+                            <span className="justify-center items-center">
+                                <img
+                                    src={kathleen.pic}
+                                    alt={kathleen.title}
+                                    className="rounded-xl"
+                                />
+                            </span>
+                            <p className="text-base text-gray-900 dark:text-gray-100 font-bold text-center">
+                                {kathleen.title}
+                            </p>
+                            <p className="text-sm text-gray-900 dark:text-gray-100 text-center">
+                                {kathleen.date}
+                            </p>
+                            <br />
+                            <br />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </main>
+    )
+};
